108834835229fc58b7dc0ffdf1b7103c7f09dae1
professor <professor@SN-371151.edu.fiesc.com.br>
2024-06-13 22:36:03 -0300

    att tela

--- milkshakespeareSim/src/TelaHome.java
+++ milkshakespeareSim/src/TelaHome.java
@@ -8,8 +8,27 @@ public class TelaHome extends JFrame{
     private JButton juniorButton;
     private JButton plenoButton;
     private JButton seniorButton;
+    private JTextArea textArea1;
+
+    private int ultimoID = 0;
 
     public TelaHome() {
+
+
+//        for(int i=0; i<pedidos.size();i++){
+//            pedidos.get(i).getId()
+//        }
+//        for(Pedido p : pedidos){
+//            p.getID()
+//        }
+
+//        Milkshake m = new MilkShake(1, "Gigante", "Ervilha");
+//
+//        pedidos.add(m);
+//
+//        Pedido p = pedidos.getFirst();
+//        m = (Milkshake) p;
+
 
 }
 }
